106960c741f68336ff73ffcaa2b8d23277e2f7ab
gar35 <grutherford@hotmail.co.nz>
2018-03-13 13:36:11 +1100

    First commit for backend return dummy data

---
+++ AnsibleAPI/AnsibleAPI/Controllers/EntityController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AnsibleAPI.Models;
+using Newtonsoft.Json;
+
+namespace AnsibleAPI.Controllers
+{
+    public class EntityController : ApiController
+    {
+        public EntityController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("v1/entities/{entityId}")]
+        public HttpResponseMessage GetEntity(int entityId)
+        {
+            var entity = new Entity()
+            {
+                Id = 1,
+                Name = "Test Entity"
+            };
+            var response = Request.CreateResponse(HttpStatusCode.OK, entity);
+            return response;
+        }
+    }
+}
