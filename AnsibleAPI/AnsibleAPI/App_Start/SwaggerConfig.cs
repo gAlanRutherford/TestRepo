c26bfe721035cbac114a51ef7c4cc638e6588686
gar35 <grutherford@hotmail.co.nz>
2018-03-18 14:46:21 +1100

    Adding Swagger documentation

---
+++ AnsibleAPI/AnsibleAPI/App_Start/SwaggerConfig.cs
@@ -0,0 +1,22 @@
+using System.Web.Http;
+using WebActivatorEx;
+using AnsibleAPI;
+using Swashbuckle.Application;
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace AnsibleAPI
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            
+            var thisAssembly = typeof(SwaggerConfig).Assembly;
+
+            GlobalConfiguration.Configuration
+                .EnableSwagger(c => c.SingleApiVersion("v1", "AnsibleAPI"))
+                .EnableSwaggerUi();
+        }
+    }
+}
